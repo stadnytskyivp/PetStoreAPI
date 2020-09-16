a26af29a203d46c57a20d47e8f48d805f3ce21fd
stadnytskyivp <stadnytskyi.volodymyr@gmail.com>
2020-09-16 15:24:14 +0300

    adding order pojo class

---
+++ src/main/java/dto/requests/store/Order.java
@@ -0,0 +1,54 @@
+package dto.requests.store;
+
+public class Order {
+    private int id;
+    private int petId;
+    private int quantity;
+    private String shipDate;
+    private String status;
+
+    public int getId() {
+        return id;
+    }
+
+    public Order setId(int id) {
+        this.id = id;
+        return this;
+    }
+
+    public int getPetId() {
+        return petId;
+    }
+
+    public Order setPetId(int petId) {
+        this.petId = petId;
+        return this;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public Order setQuantity(int quantity) {
+        this.quantity = quantity;
+        return this;
+    }
+
+    public String getShipDate() {
+        return shipDate;
+    }
+
+    public Order setShipDate(String shipDate) {
+        this.shipDate = shipDate;
+        return this;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public Order setStatus(String status) {
+        this.status = status;
+        return this;
+    }
+}
